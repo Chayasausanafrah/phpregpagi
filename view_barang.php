6933c1ec270ad65a1d44eae362b7ca2602d38710
chayasausanafrah <chayatuankotta@gmail.com>
2021-11-08 20:58:55 +0700

    up view_barang.php ubah

--- view_barang.php
+++ view_barang.php
@@ -3,12 +3,14 @@ include ("koneksi.php");
 $query_view=mysqli_query($koneksi,"select * from barang");
 
 ?>
+</br>
+<a href="input_barang.php" class="btn btn-danger">Tambah barang</a>
 <table class="table table-bordered" border="1">
   <tr>
     <td>No</td>
     <td>id_kategori</td>
     <td>Nama</td>
-    
+    <td colspan="4">Action</td>
   </tr>
   <?php
   $no=1;
@@ -17,6 +19,8 @@ $query_view=mysqli_query($koneksi,"select * from barang");
       <td><?php echo $no++;?></td>
       <td><?php echo $tampil ['id_kategori'];?></td>
       <td><?php echo $tampil ['nama'];?></td>
+      <td><a href="edit_barang.php?id_barang=<?php echo $tampil ['id_barang'];?>">Edit</a></td>
+      <td><a href="hapus_barang.php?id_barang=<?php echo $tampil ['id_barang'];?>">hapus</a></td>
   </tr>
 <?php }?>
 
