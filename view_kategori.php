0c7c72f1c3c8ddd18ce61ef1f9e9f533835fc13c
Chayasausanafrah <91579503+Chayasausanafrah@users.noreply.github.com>
2021-11-16 05:16:48 +0700

    menambahkan codingan

--- view_kategori.php
+++ view_kategori.php
@@ -1,15 +1,23 @@
 <?php
 include ("koneksi.php");
+session_start();
+if(!($_SESSION['username'])){
+  echo "<SCRIPT>alert('silahkan login terlebih dahulu !');window.location='index.php'</SCRIPT>";
+}
 $query_view=mysqli_query($koneksi,"select * from kategori");
 
 ?>
 </br>
-<a href="input_barang.php" class="btn btn-danger">Tambah barang</a>
+<?php
+if($_SESSION['level']=="admin"){
+    echo '<a href="input_kategori.php" class="btn btn-danger">Tambah kategori</a>';
+}?> <a href="home.php">Halaman Utama</a>
 <table class="table table-bordered" border="1">
   <tr>
     <td>No</td>
     <td>Nama</td>
     <td colspan="4">Action</td>
+    <?php if($_SESSION['level']=="admin")?>
     
   </tr>
   <?php
@@ -18,8 +26,8 @@ $query_view=mysqli_query($koneksi,"select * from kategori");
     <tr>
       <td><?php echo $no++;?></td>
       <td><?php echo $tampil ['nama'];?></td>
-      <td><a href="edit_kategori.php?id_kategori=<?php echo $tampil ['id_kategori'];?>">Edit</a></td>
-      <td><a href="hapus_kategori.php?id_kategori=<?php echo $tampil ['id_kategori'];?>">hapus</a></td>
+      <?php if($_SESSION['level']=="admin"){ echo '<td><a href="edit_kategori.php?id_kategori='.$tampil ['id_kategori'].'">Edit</a></td>';
+      echo '<td><a href="hapus_kategori.php?id_kategori='.$tampil ['id_kategori'].'" onclick="return confirm("yakin keluar?")">hapus</a></td>';}?>
   </tr>
 <?php }?>
 
